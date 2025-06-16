f6de2f9b47208340c4e5452d9df12386a97fa9cc
= <=>
2025-06-16 23:30:47 +0700

    fix

--- 03-buffalo-async-replication/entrypoint.sh
+++ 03-buffalo-async-replication/entrypoint.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-# Cek apakah sudah ada project
 if [ ! -f /app/main.go ]; then
   echo "🌀 Cloning project dari GitHub..."
   git clone https://github.com/lukman-ss/lab.git /tmp/lab
-
   cp -r /tmp/lab/03-buffalo-async-replication/. /app/
   rm -rf /tmp/lab
 else
@@ -15,19 +13,15 @@ fi
 
 cd /app
 
-# Tunggu PostgreSQL master ready
 echo "⏳ Menunggu database..."
 until pg_isready -h postgre-sync-master -p 5432 -U postgres; do
   sleep 1
 done
 
-# Export DATABASE_URL agar dikenali `buffalo pop`
 export DATABASE_URL=postgres://postgres:example@postgre-sync-master:5432/postgres?sslmode=disable
 
-# Jalankan migrasi
 echo "⚙️  Menjalankan migrasi..."
 buffalo pop migrate
 
-# Jalankan aplikasi
 echo "🚀 Menjalankan Buffalo Dev..."
 buffalo dev
