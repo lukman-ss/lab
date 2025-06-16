5597c240120e47df4b0ad2d631ef1facfd82b6df
= <=>
2025-06-16 23:07:44 +0700

    update dockerfile

--- 03-buffalo-async-replication/entrypoint.sh
+++ 03-buffalo-async-replication/entrypoint.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-# Cek apakah main.go ada
+# Cek apakah sudah ada project
 if [ ! -f /app/main.go ]; then
-  echo "🌀 Project belum ada, cloning dari repo..."
+  echo "🌀 Cloning project dari GitHub..."
   git clone https://github.com/lukman-ss/lab.git /tmp/lab
 
-  mkdir -p /app
   cp -r /tmp/lab/03-buffalo-async-replication/. /app/
   rm -rf /tmp/lab
 else
-  echo "✅ Project sudah ada, skip git clone"
+  echo "✅ Project sudah ada, skip clone"
 fi
 
 cd /app
 
-# Tunggu database siap
+# Tunggu PostgreSQL master ready
 echo "⏳ Menunggu database..."
 until pg_isready -h postgre-sync-master -p 5432 -U postgres; do
   sleep 1
 done
 
-# Jalankan migrasi (optional)
-echo "⚙️  Menjalankan migrasi database..."
+# Export DATABASE_URL agar dikenali `buffalo pop`
+export DATABASE_URL=postgres://postgres:example@postgre-sync-master:5432/postgres?sslmode=disable
+
+# Jalankan migrasi
+echo "⚙️  Menjalankan migrasi..."
 buffalo pop migrate
 
 # Jalankan aplikasi
-echo "🚀 Menjalankan Buffalo dev..."
+echo "🚀 Menjalankan Buffalo Dev..."
 buffalo dev
